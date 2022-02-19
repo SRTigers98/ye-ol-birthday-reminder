d720393795e07c10ed1d806e25b7026b422668d5
Eder Benjamin <ben-eder@freenet.de>
2022-02-19 14:34:53 +0100

    Initialized discord bot

--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/BirthdayDiscordBotApplication.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/BirthdayDiscordBotApplication.kt
@@ -1,18 +1,32 @@
 package io.github.srtigers98.birthdaydiscordbot.application
 
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BirthdayDiscordBotApplication : CommandLineRunner {
+class BirthdayDiscordBotApplication(
+  @Value("\${bot.token}") private val token: String
+) : CommandLineRunner {
 
-  val log = LoggerFactory.getLogger(BirthdayDiscordBotApplication::class.java) as Logger
+  private val log: Logger = LoggerFactory.getLogger(BirthdayDiscordBotApplication::class.java)
 
-  override fun run(vararg args: String?) {
-    log.info("Hello World!")
+  override fun run(vararg args: String?) = runBlocking {
+    val client = Kord(token)
+
+    client.on<MessageCreateEvent> {
+      log.info("Message received: ${message.content}")
+    }
+
+    log.info("Discord Bot started!")
+    client.login()
   }
 }
 
