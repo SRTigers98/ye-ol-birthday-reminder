8acb9af57c3741b546ab22a9432a51cbd6e7534f
Eder Benjamin <ben-eder@freenet.de>
2022-02-19 14:06:40 +0100

    Changed to command line runner application

--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/BirthdayDiscordBotApplication.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/BirthdayDiscordBotApplication.kt
@@ -1,10 +1,20 @@
 package io.github.srtigers98.birthdaydiscordbot.application
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BirthdayDiscordBotApplication
+class BirthdayDiscordBotApplication : CommandLineRunner {
+
+  val log = LoggerFactory.getLogger(BirthdayDiscordBotApplication::class.java) as Logger
+
+  override fun run(vararg args: String?) {
+    log.info("Hello World!")
+  }
+}
 
 fun main(args: Array<String>) {
   runApplication<BirthdayDiscordBotApplication>(*args)
