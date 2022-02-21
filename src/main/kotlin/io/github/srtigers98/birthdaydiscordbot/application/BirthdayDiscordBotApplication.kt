8c0efeb4f4dbfdeecd12fe06bdc796ea439bad1f
Benjamin Eder <ben-eder@freenet.de>
2022-02-21 15:07:26 +0100

    feat: Command prototype

--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/BirthdayDiscordBotApplication.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/BirthdayDiscordBotApplication.kt
@@ -1,8 +1,14 @@
 package io.github.srtigers98.birthdaydiscordbot.application
 
+import dev.kord.common.entity.InteractionResponseType
+import dev.kord.common.entity.optional.Optional
+import dev.kord.common.entity.optional.map
 import dev.kord.core.Kord
-import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
 import dev.kord.core.on
+import dev.kord.rest.builder.interaction.StringChoiceBuilder
+import dev.kord.rest.json.request.InteractionApplicationCommandCallbackData
+import dev.kord.rest.json.request.InteractionResponseCreateRequest
 import io.github.srtigers98.birthdaydiscordbot.application.service.MessageService
 import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
@@ -25,9 +31,29 @@ class BirthdayDiscordBotApplication(
   override fun run(vararg args: String?) = runBlocking {
     val client = Kord(token)
 
-    client.on<MessageCreateEvent> {
-      if (message.content.startsWith("!") && message.author?.isBot == false) {
-        messageService.handleMessage(message)
+    // Clear all previous commands
+    client.rest.interaction.getGlobalApplicationCommands(client.selfId).forEach {
+      client.rest.interaction.deleteGlobalApplicationCommand(it.applicationId, it.id)
+    }
+
+    val bdayIsCommand = client.createGlobalChatInputCommand("bdayis", "Saves your birthday to the server.") {
+      options = mutableListOf(
+        StringChoiceBuilder("birthday", "Your birthday.").apply {
+          required = true
+        }
+      )
+    }
+
+    client.on<ChatInputCommandInteractionCreateEvent> {
+      if (interaction.data.data.name.map { it == bdayIsCommand.name }.value == true) {
+        log.info(interaction.data.data.options.toString())
+        bdayIsCommand.service.createInteractionResponse(
+          interaction.id, interaction.token,
+          InteractionResponseCreateRequest(
+            InteractionResponseType.ChannelMessageWithSource,
+            Optional.invoke(InteractionApplicationCommandCallbackData(content = Optional.invoke("Reacted to the command!")))
+          )
+        )
       }
     }
 
