76c83574ef589b517dee7e59ded56a88b3e3c2b0
Benjamin Eder <ben-eder@freenet.de>
2022-02-25 17:28:49 +0100

    feat: Added postgresql driver

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.postgresql:postgresql")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("dev.kord:kord-core:0.8.0-M10")
