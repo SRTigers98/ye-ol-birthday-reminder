eb2a53e9357b5de417816eb272f603ee8ac8186f
Benjamin Eder <ben-eder@freenet.de>
2022-03-01 22:22:29 +0100

    docs: Added javadoc for dto and dao classes

--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dao/BirthdayRepository.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dao/BirthdayRepository.kt
@@ -5,6 +5,11 @@ import io.github.srtigers98.birthdaydiscordbot.application.dto.BirthdayId
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
+/**
+ * Data access object to access the birthday entities in the database.
+ *
+ * @author Benjamin Eder
+ */
 @Repository
 interface BirthdayRepository : JpaRepository<Birthday, BirthdayId> {
 
--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dto/BirthdayId.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dto/BirthdayId.kt
@@ -2,6 +2,11 @@ package io.github.srtigers98.birthdaydiscordbot.application.dto
 
 import java.io.Serializable
 
+/**
+ * The key for a birthday entry.
+ *
+ * @author Benjamin Eder
+ */
 data class BirthdayId(
   val userId: String = "",
   val guild: String = ""
