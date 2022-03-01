50429ca95c8751ee115e6089bf9deea7a4eaddb7
Benjamin Eder <ben-eder@freenet.de>
2022-03-01 22:14:31 +0100

    docs: Added javadoc for application, config and exceptions

--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/config/DiscordConfiguration.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/config/DiscordConfiguration.kt
@@ -7,16 +7,27 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/**
+ * Configuration for kord beans.
+ *
+ * @author Benjamin Eder
+ */
 @Configuration
 class DiscordConfiguration(
   @Value("\${bot.token}") private val token: String
 ) {
 
+  /**
+   * Creates a kord instance to interact with the _Discord API_ with the configured token for the bot.
+   */
   @Bean
   fun kord(): Kord = runBlocking {
     return@runBlocking Kord(token)
   }
 
+  /**
+   * Creates a rest client to interact with the _Discord REST API_ with the configured token for the bot.
+   */
   @Bean
   fun restClient(): RestClient = RestClient(token)
 }
