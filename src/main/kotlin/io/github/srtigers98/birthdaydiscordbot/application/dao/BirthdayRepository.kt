8fdc3d4210d560abed1753340484f3ad71039001
Benjamin Eder <ben-eder@freenet.de>
2022-03-01 22:20:00 +0100

    fix: Added repository annotation to dao repositories

--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dao/BirthdayRepository.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dao/BirthdayRepository.kt
@@ -3,7 +3,9 @@ package io.github.srtigers98.birthdaydiscordbot.application.dao
 import io.github.srtigers98.birthdaydiscordbot.application.dto.Birthday
 import io.github.srtigers98.birthdaydiscordbot.application.dto.BirthdayId
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface BirthdayRepository : JpaRepository<Birthday, BirthdayId> {
 
   fun findByBirthdayMonthIsAndBirthdayDayIs(month: Int, day: Int): List<Birthday>
--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dao/GuildConfigRepository.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/dao/GuildConfigRepository.kt
@@ -2,5 +2,7 @@ package io.github.srtigers98.birthdaydiscordbot.application.dao
 
 import io.github.srtigers98.birthdaydiscordbot.application.dto.GuildConfig
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface GuildConfigRepository : JpaRepository<GuildConfig, String>
