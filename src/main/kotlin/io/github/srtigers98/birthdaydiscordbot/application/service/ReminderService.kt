094f0b4502bba8b7ab32b45ee85b7aa2c6dd4889
Stephan Brunner <s.brunner@stephan-brunner.net>
2022-03-07 19:25:20 +0100

    feat: Add logging for ReminderService
    Fixes #5

--- src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/service/ReminderService.kt
+++ src/main/kotlin/io/github/srtigers98/birthdaydiscordbot/application/service/ReminderService.kt
@@ -3,9 +3,12 @@ package io.github.srtigers98.birthdaydiscordbot.application.service
 import dev.kord.common.entity.Snowflake
 import dev.kord.rest.service.RestClient
 import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 /**
  * Service to read the saved birthdays every day and congratulate the guild member (reminds the other guild members).
@@ -18,6 +21,9 @@ class ReminderService(
   private val birthdayService: BirthdayService
 ) {
 
+  private val log: Logger = LoggerFactory.getLogger(ReminderService::class.java)
+  private val logDateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
   /**
    * Check every day at 12 pm if any saved birthday is today.
    * If there are birthdays today, send a congratulation message to the guild member in the configured birthday channel in the guild.
@@ -25,6 +31,9 @@ class ReminderService(
   @Scheduled(cron = "0 0 12 * * *")
   fun checkForBirthday() {
     val today = LocalDate.now()
+
+    log.info("Checking for birthdays on {}", logDateFormatter.format(today))
+
     val birthdays = birthdayService.checkForBirthdayOn(today)
 
     birthdays.forEach {
