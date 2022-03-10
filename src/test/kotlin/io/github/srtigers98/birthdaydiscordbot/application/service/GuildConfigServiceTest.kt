20b9cd7f03580f9822bea627f717ecac10558d49
Benjamin Eder <ben-eder@freenet.de>
2022-03-10 20:02:12 +0100

    feat: Extended guild config service #2

--- src/test/kotlin/io/github/srtigers98/birthdaydiscordbot/application/service/GuildConfigServiceTest.kt
+++ src/test/kotlin/io/github/srtigers98/birthdaydiscordbot/application/service/GuildConfigServiceTest.kt
@@ -3,16 +3,16 @@ package io.github.srtigers98.birthdaydiscordbot.application.service
 import io.github.srtigers98.birthdaydiscordbot.application.dao.GuildConfigRepository
 import io.github.srtigers98.birthdaydiscordbot.application.dto.GuildConfig
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
-import org.mockito.kotlin.times
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
+import org.mockito.kotlin.*
 import java.util.*
+import javax.persistence.EntityManager
 
 @ExtendWith(MockitoExtension::class)
 internal class GuildConfigServiceTest {
@@ -22,6 +22,9 @@ internal class GuildConfigServiceTest {
 
   @Mock
   private lateinit var guildConfigRepository: GuildConfigRepository
+
+  @Mock
+  private lateinit var entityManager: EntityManager
 
   @Test
   fun selectGuildChannelTest() {
@@ -68,5 +71,37 @@ internal class GuildConfigServiceTest {
     val result = tested.getGuildConfig(guildId, currentChannelId)
 
     assertThat(result, `is`(GuildConfig(guildId, currentChannelId)))
+  }
+
+  @Test
+  fun getAllGuildConfigsTest() {
+    val config1: GuildConfig = mock()
+    val config2: GuildConfig = mock()
+
+    whenever(guildConfigRepository.findAll())
+      .thenReturn(listOf(config1, config2))
+
+    val result = tested.getAllGuildConfigs()
+
+    assertThat(result.size, `is`(2))
+    assertThat(result, hasItems(config1, config2))
+  }
+
+  @Test
+  fun deleteGuildConfigTest() {
+    val config1: GuildConfig = mock()
+    val config2: GuildConfig = mock()
+
+    whenever(guildConfigRepository.deleteAll(any()))
+      .thenAnswer { }
+    whenever(entityManager.clear())
+      .thenAnswer { }
+
+    tested.deleteGuildConfig(config1, config2)
+
+    verify(guildConfigRepository, times(1))
+      .deleteAll(listOf(config1, config2))
+    verify(entityManager, times(1))
+      .clear()
   }
 }
