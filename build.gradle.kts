da07d1fe875d36e92e81f4baa21acf462965c58a
Benjamin Eder <eder.ben01@gmail.com>
2024-08-26 20:57:07 +0200

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.springframework.boot.gradle.dsl.SpringBootExtension
 plugins {
   id("org.springframework.boot") version "3.3.3"
   id("io.spring.dependency-management") version "1.1.6"
-  kotlin("jvm") version "2.0.10"
-  kotlin("plugin.spring") version "2.0.10"
+  kotlin("jvm") version "2.0.20"
+  kotlin("plugin.spring") version "2.0.20"
   kotlin("plugin.jpa") version "2.0.20"
   id("org.sonarqube") version "5.1.0.4882"
   id("jacoco")
